3ef75586ec6c6d4af87f140e47f7739966474150
Martin Stacey <martin__stacey@hotmail.com>
2021-12-26 19:53:57 +0000

    touches1

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
 function setup() {
   createCanvas(800, 800);
-  fullscreen();
+  //fullscreen();
 }
 
 function draw() {
-  background(255,0,0);
+  clear();
+  background(255,255,0);
   circle(mouseX,mouseY,100)
+  let display = touches.length + ' touches';
+  text(display, 5, 10);
+}
+
+function mousePressed() {
+  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
 }
