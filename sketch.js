f8a1591c5c60342a92c6088456f72d7d6f0bf6ba
Martin Stacey <martin__stacey@hotmail.com>
2021-12-26 20:08:42 +0000

    skate2

--- sketch.js
+++ sketch.js
@@ -1,19 +1,13 @@
 function setup() {
-  createCanvas(800, 800);
-  //fullscreen();
+  createCanvas(displayWidth, displayHeight);
 }
 
 function draw() {
   clear();
-  background(255,255,0);
+  background(0,255,0);
   circle(mouseX,mouseY,100)
   let display = touches.length + ' touches';
+  textSize(32);
   text(display, 5, 10);
-}
-
-function mousePressed() {
-  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
-    let fs = fullscreen();
-    fullscreen(!fs);
-  }
+  console.log(touches);
 }
