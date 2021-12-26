2a74162f5bcbc2eadc9b491579f266d3c23ed3a8
Martin Stacey <martin__stacey@hotmail.com>
2021-12-26 21:16:26 +0000

    touch1

--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
+let restore = false;
+
 function setup() {
   createCanvas(displayWidth, displayHeight);
 }
 
 function draw() {
   clear();
-  background(255,0,0);
-  circle(mouseX,mouseY,100)
+  background(255,0,255);
+  //circle(mouseX,mouseY,100)
   let display = touches.length + ' touches';
   textSize(32);
-  text(display, 5, 10);
-  console.log(touches);
+  text(display, width/2, height/2);
+  if (touches.length>1){
+    let x0 = touches[0];
+    circle(touches[0].x,touches[0].y,100)
+    circle(touches[1].x,touches[1].y,100)
+  }
+
+}
+
+function keyPressed(){
+restore = restore;
+console.log(touches[0].x);
 }
